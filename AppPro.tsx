fdaf3e9ff862f325283479dce53658ce4390a407
Gokul <36274337+wolfiee27@users.noreply.github.com>
2024-11-29 14:36:01 +0530

    initialize a new component AppPro instead of App

---
+++ AppPro.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+const AppPro = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <View>
+          <Text>Hello World</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default AppPro;
