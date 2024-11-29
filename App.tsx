444e0185a09fed7afe36e0d3db53d018b47db436
Gokul <36274337+wolfiee27@users.noreply.github.com>
2024-11-29 14:24:21 +0530

    Project setup

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <View>
+          <Text> Hello World!!! </Text>
+          <Text> Hello World!!! </Text>
+          <Text> Hello World!!! </Text>
+          <Text> Hello World!!! </Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
