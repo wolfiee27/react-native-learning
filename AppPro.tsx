9a0b624a3d8325e551c3f0be0ad9c83833500bbd
Gokul <36274337+wolfiee27@users.noreply.github.com>
2024-11-29 15:12:18 +0530

    using stylesheet and useColorScheme
    useColorScheme is a hook provided by RN to identify the user color setting preference

--- AppPro.tsx
+++ AppPro.tsx
@@ -1,16 +1,46 @@
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  useColorScheme,
+} from 'react-native';
 
-const AppPro = () => {
+const AppPro: React.FC = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
   return (
     <>
-      <SafeAreaView>
-        <View>
-          <Text>Hello World</Text>
+      <SafeAreaView style={styles.safeAreaView}>
+        <View style={styles.container}>
+          <Text style={isDarkMode ? styles.whiteText : styles.darkText}>
+            Hello World
+          </Text>
         </View>
       </SafeAreaView>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  safeAreaView: {
+    flex: 1,
+  },
+  textStyle: {
+    alignItems: 'center',
+    display: 'flex',
+  },
+  whiteText: {
+    color: '#FFFFFF',
+  },
+  darkText: {
+    color: '#000000',
+  },
+});
 
 export default AppPro;
